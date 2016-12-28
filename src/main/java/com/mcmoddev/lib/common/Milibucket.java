d5a82a02395c85c8738f16d66974ed7a7fbe7d54
Tyler Hancock <darklime@live.ca>
2016-12-28 10:39:24 -0700

    Added a few more things from Bookshelf.  (#5)
    * Added constants for all oredict entries.
    
    * Added a graphical gui button.
    
    * Added milibucket standard measurements.
    
    * Basic enchantment object representation.
    
    * Added the weighted selector.

---
+++ src/main/java/com/mcmoddev/lib/common/Milibucket.java
@@ -0,0 +1,30 @@
+package com.mcmoddev.lib.common;
+
+/**
+ * Contains universal measurements for the milibucket (mb) standard.
+ *
+ * @author Tyler Hancock (Darkhax)
+ */
+public enum Milibucket {
+
+    NUGGET(16),
+    INGOT(144),
+    Bottle(333),
+    BUCKET(1000),
+    BLOCK(1296);
+
+    /**
+     * The amount of milibuckets (mb) for the measurement.
+     */
+    public final int amount;
+
+    /**
+     * Enum constructor.
+     *
+     * @param amount The amount of milibuckets (mb) in the measurement.
+     */
+    Milibucket(int amount) {
+
+        this.amount = amount;
+    }
+}
